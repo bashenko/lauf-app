e973c46a24b84b7fc36956136bab0b985e1c9141
bashenko <kb@neft.cc>
2019-10-04 14:04:32 +0500

    pace calculated, distance calculated

--- App.js
+++ App.js
@@ -1,64 +1,116 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import React, { Component } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import haversine from 'haversine';
 
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      location: null,
+      errorMessage: null,
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
 
-import AppNavigator from './navigation/AppNavigator';
+  componentWillMount() {
+    var _this=this;
+    Location.watchPositionAsync({
+enableHighAccuracy:true, timeInterval:50, activityType: Location.ActivityType.Fitness,
+    }, location2 => {
+      _this.setState({
+        location2, 
+        speed: location2.coords.speed, 
+        long: location2.coords.longitude, 
+        lat: location2.coords.latitude,
+        start: {
+          latitude: this.state.initLat, 
+          longitude: this.state.initLong,
+        },
+        end: {
+          latitude: this.state.lat, 
+          longitude: this.state.long,
+        },
+        distance: haversine({latitude: this.state.initLat, longitude: this.state.initLong}, {latitude: this.state.lat, 
+          longitude: this.state.long,}, {unit: 'meter'}),
+      });
+      console.log(this.state.start)
+    });
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ 
+      location, 
+      initLat: location.coords.latitude, 
+      initLong: location.coords.longitude,
+    });
+  };
+
+  componentDidMount() {
+    this.watchId = navigator.geolocation.watchPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 100, maximumAge: 0, distanceFilter: 1},
     );
-  } else {
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
+  }
+
+  
+
+  render() {
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
+      <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Initial Latitude: {this.state.initLat}</Text>
+        <Text>Initial Longitude: {this.state.initLong}</Text>
+        <Text>Ongoing Latitude: {this.state.lat}</Text>
+        <Text>Ongoing Longitude: {this.state.long}</Text>
+       
+        {this.state.distance ? <Text>Distance since start: {Math.floor(this.state.distance*100)/100}m</Text> : <Text>Distance since start: 0m</Text>}
+        
+        
+        <Text>Speed:  {this.state.speed} meters per second</Text>
+        <Text>{Math.floor(100/60) + " h"}, {100%60 + " min" }</Text>
+        <Text> {Math.round(this.state.speed * Math.pow(60,2))} meters in hour</Text>
+        <Text> {Math.round(this.state.speed * Math.pow(60,2)/10)/100} kilometers in hour</Text>
+
+        <Text>Pace {Math.round(1000/this.state.speed/60*100)/100} min/KM</Text>
+        <Text>
+        {Math.floor(1000/this.state.speed / 60)+ ':' + Math.floor(1000/this.state.speed % 60)} min/km
+        </Text>    
+
+        <Text>Onoging Position: {JSON.stringify(this.state.location2)}</Text>
+        {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
       </View>
     );
   }
 }
-
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
-}
-
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # lauf-app
 Running tracking app
+
+
+location: https://www.youtube.com/watch?v=UcWG2o2gVzw
+https://blog.expo.io/a-complete-guide-to-recording-location-data-in-react-native-9ee6e1ef7893
+
+Distance calculation with HAVERSINE:
+https://www.npmjs.com/package/haversine
+
+
+*Update position with EXPO:*
+watchPositionAsync
+
+*UpdatePosition with Native:*
+navigator.geolocation.watchPosition
+
+location.coords.speed — meters per second
+
+pace (min/km) = 
+speed (km/h) = floor(1000 / coords.speed) + speed % 60
